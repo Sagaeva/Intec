1418dca2c5aabf279a8fc131e9da4641debfcd1c
Yana <98331675+Sagaeva@users.noreply.github.com>
2022-02-14 20:25:37 +0100

    Add files via upload

--- ArrayApp.java
+++ ArrayApp.java
@@ -1,58 +1,28 @@
-package be.intecbrussel.arrays;
+package com.company;
+
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 
 public class ArrayApp {
 
+
     public static void main(String[] args) {
 
-        int[] arr = {1, 6, 9, 222, 2, -3, 26};
-        int arrzondersort = arr[0];
-        System.out.println("Origineel volgorde is:" + Arrays.toString(arr));
-        Arrays.sort(arr);
+        int[] myArray = {1,222, 9, 6};
 
-        System.out.println("Gesorteerde volgorde is:" + Arrays.toString(arr));
-        int arrmetsort = arr[0];
-
-        boolean isDone = arrzondersort != arrmetsort;
-
-        System.out.println(isDone);
+        
 
 
-        // opdracht 2
+        System.out.println(IntArrayTools.isSorted(myArray));
 
-        Integer[] arr2 = {3, 6, 9, -5, 100, 324, 7};
 
-        Arrays.sort(arr2, Collections.reverseOrder());
-        for (int i = 0; i < arr2.length; i++) {
 
-        }
-        System.out.println("Omgekeerde volgorde\t" + Arrays.toString(arr2));
+    }
 
 
 
 
 
 
-
-
-
-        }
-
-
-
-                }
-
-
-
-
-
-
-
-
-
-
-
+}
 
